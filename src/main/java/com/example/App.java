eb1d0b409eac820472cbc34b7faf0f25cee7b05a
shraddha1231 <132136377+shraddha1231@users.noreply.github.com>
2026-06-17 15:29:52 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,19 +3,29 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com");
-        driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        System.out.println("Login successful");
+
+        Thread.sleep(3000);
+
+        driver.quit();
     }
 }
