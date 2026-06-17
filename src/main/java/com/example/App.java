0fe7f61488c43e047c67822109d8d58a9a6621f9
shraddha1231 <132136377+shraddha1231@users.noreply.github.com>
2026-06-17 15:40:22 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,9 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         ChromeOptions options = new ChromeOptions();
+
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
@@ -17,15 +18,12 @@ public class App {
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com");
+        driver.manage().window().maximize();
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
 
-        System.out.println("Login successful");
-
-        Thread.sleep(3000);
-
-        driver.quit();
+        driver.quit();   // important (prevents “auto close issue” later)
     }
 }
